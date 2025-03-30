854a2681efda8594072330805ea6d1c79360857e
NataliaPontos <pontos.natalia@yahoo.com>
2025-03-30 14:17:23 +0300

    Initial commit

---
+++ src/test/java/pageLocators/IndexLocators.java
@@ -0,0 +1,8 @@
+package pageLocators;
+
+import org.openqa.selenium.By;
+
+public class IndexLocators {
+    public static final By bankManagerLoginMenu = By.xpath("//button[normalize-space()='Bank Manager Login']");
+
+}
